e772ee631b8a9c19e216bac92ad3a89cf487df9c
unknown <ritesh7785mishra@gmail.com>
2023-01-12 15:40:41 +0530

    Implementing controller for plans

--- foodApp/Routers/planRouter.js
+++ foodApp/Routers/planRouter.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const planRouter = express.Router();
 const { protectRoute, isAuthorised } = require("../controller/authController");
+const {
+  createPlan,
+  getAllPlans,
+  getPlan,
+  updatePlan,
+  deletePlan,
+} = require("../controller/planController");
 
 //all plants
 planRouter.route("/allPlans").get(getAllPlans);
@@ -11,8 +18,6 @@ planRouter.route("/plan/:id").get(getPlan);
 
 // admin and restaurant owneers can only create, update or delete plans
 planRouter.use(isAuthorised[("admin", "restaurantowner")]);
-planRouter
-  .route("/crudPlan")
-  .post(createPlan)
-  .patch(updatePlan)
-  .delete(deletePlan);
+planRouter.route("/crudPlan").post(createPlan);
+
+planRouter.route("/crudPlan/:id").patch(updatePlan).delete(deletePlan);
---
+++ foodApp/controller/planController.js
@@ -0,0 +1,125 @@
+const planModel = require("../models/planModel");
+
+module.exports.getAllPlans = async function getAllPlans(req, res) {
+  try {
+    let plans = await planModel.find();
+    if (plans) {
+      return res.json({
+        message: "all plans retrieved",
+        data: plans,
+      });
+    } else {
+      return res.json({
+        message: "plans not found",
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+module.exports.getPlan = async function getPlan(req, res) {
+  try {
+    let id = req.params.id;
+    let plan = await planModel.findById(id);
+    if (plan) {
+      return res.json({
+        message: "plan retrieved",
+        data: plan,
+      });
+    } else {
+      return res.json({
+        message: "plan not found",
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+module.exports.createPlan = async function createPlan(req, res) {
+  try {
+    let planData = req.body;
+    let createdPlan = await planModel.create(planData);
+    return res.json({
+      message: "plan created Successfully",
+      data: createdPlan,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: err.message,
+    });
+  }
+};
+
+module.exports.deletePlan = async function deletePlan(req, res) {
+  try {
+    let id = req.params.id;
+    let deletedPlan = await planModel.findByIdAndDelete(id);
+    return res.json({
+      message: "plan deleted Successfully",
+      data: deletedPlan,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: err.message,
+    });
+  }
+};
+
+module.exports.updatePlan = async function updatePlan(req, res) {
+  try {
+    let id = req.params.id;
+    let dataToBeUpdated = req.body;
+    let keys = [];
+    for (let key in dataToBeUpdated) {
+      keys.push(key);
+    }
+
+    let plan = await planModel.findById(id);
+    for (let key in keys) {
+      plan[key] = dataToBeUpdated[key];
+    }
+    await plan.save();
+    return res.json({
+      message: "plan deleted Successfully",
+      data: deletedPlan,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: err.message,
+    });
+  }
+};
+
+//get top 3 plans
+
+module.exports.top3Plans = async function top3Plans(req, res) {
+  try {
+    const plans3 = await planModel
+      .find()
+      .sort({
+        ratingsAverage: -1,
+      })
+      .limit(3);
+
+    if (plans3) {
+      return res.json({
+        message: "here are the top 3 plans ",
+        data: plans3,
+      });
+    } else {
+      return res.json({
+        message: "plans not found",
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: err.message,
+    });
+  }
+};
